ff2df11dd865fd2989f0f1d1877b1147d7d20cbf
14j123 <14j123@gmail.com>
2020-09-28 23:03:42 +0800

    Improve burger builder

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -8,6 +8,7 @@ import axios from '../../../axios-orders';
 import Input from '../../../components/UI/Input/Input';
 import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler';
 import * as actions from '../../../store/actions/index';
+import { updateObject, checkValidity } from '../../../shared/utility'; 
 
 class ContactData extends Component {
     state={
@@ -110,35 +111,15 @@ class ContactData extends Component {
         this.props.onOrderBurger(order, this.props.token);
     }
 
-    checkValidity (value, rules) {
-        let isValid = true;
-
-        if(!rules){
-            return true;
-        }
-
-        if (rules.required){
-            isValid = value.trim() !== '' && isValid;
-        }
-
-        if (rules.minLength){
-            isValid = value.length >= rules.minLength && isValid;
-        }
-
-        if (rules.maxLength){
-            isValid = value.length <= rules.maxLength && isValid;
-        }
-
-        return isValid;
-    }
-
     inputChangedHandler = (event, inputIdentifier) =>{
-        const updatedOrderForm = {...this.state.orderForm};
-        const updatedOrderFormElem = {...this.state.orderForm[inputIdentifier]};
-        updatedOrderFormElem.value = event.target.value;
-        updatedOrderFormElem.touched = true;
-        updatedOrderFormElem.valid = this.checkValidity(updatedOrderFormElem.value, updatedOrderFormElem.validation);
-        updatedOrderForm[inputIdentifier] = updatedOrderFormElem;
+        const updatedOrderFormElem = updateObject(this.state.orderForm[inputIdentifier], {
+            value: event.target.value,
+            touched: true,
+            valid: checkValidity(event.target.value, this.state.orderForm[inputIdentifier])
+        });
+        const updatedOrderForm = updateObject(this.state.orderForm, {
+            [inputIdentifier]: updatedOrderFormElem
+        });
         
         let formIsValid = true;
 
